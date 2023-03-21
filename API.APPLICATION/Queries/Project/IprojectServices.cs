3de80afbe4a2ec1cd4fb163ce61c5a1eed270a20
DESKTOP-2LILVUD\MSI <transon.it24@gmail.com>
2023-03-21 21:58:11 +0700

    ++ Update project

---
+++ API.APPLICATION/Queries/Project/IprojectServices.cs
@@ -0,0 +1,22 @@
+﻿using API.INFRASTRUCTURE.DataConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.APPLICATION.Queries.Project
+{
+    public interface IprojectServices
+    {
+    }
+    public class ProjectServices : IprojectServices
+    {
+        public readonly DapperContext _context;
+
+        public ProjectServices(DapperContext context)
+        {
+            _context = context;
+        }
+    }
+}
